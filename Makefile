47caa9d8c99597de6896816d3f478d5710c9c7e2
Rodrigo Manhães <rmanhaes@gmail.com>
2011-06-27 02:56:39 -0300

    removed tons of setup code from doctest files

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 all: test
 
-dependencies: should-dsl splinter specloud flask coverage
+dependencies: should-dsl splinter flask
 
 clean:
 	@find . -name "*.pyc" -delete
-
-specloud:
-	@python -c 'import specloud' 2>/dev/null || pip install specloud
 
 flask:
 	@python -c 'import flask' 2>/dev/null || pip install flask
@@ -17,12 +14,7 @@ splinter:
 should-dsl:
 	@python -c 'import should_dsl' 2>/dev/null || pip install https://github.com/hugobr/should-dsl/tarball/master
 
-coverage:
-	@python -c 'import coverage' 2>/dev/null || pip install coverage
-
-which = 'tests'
-
 test: dependencies clean
 	@echo "Running all tests..."
-	specloud --nocapture --with-coverage --cover-erase --cover-inclusive --cover-package=should_web --tests=$(which)
+	./run_all_examples.py
 
--- run_all_examples.py
+++ run_all_examples.py
@@ -4,7 +4,11 @@ import unittest
 import os
 import sys
 import glob
+from should_dsl import should, should_not
+from splinter.browser import Browser
+from tests.fake_webapp import start_server, stop_server, EXAMPLE_APP
 
+browser = Browser()
 
 def test_suite():
     flags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
@@ -18,11 +22,24 @@ def test_suite():
         if doctest_file.endswith('.txt'):
             suite.addTest(doctest.DocFileSuite(os.path.join(doctests_path,
                                                             doctest_file),
+                                               globs={'browser': browser,
+                                                      'should': should,
+                                                      'should_not': should_not},
                                                optionflags=flags))
     return suite
 
+
+def setup():
+    start_server()
+    browser.visit(EXAMPLE_APP)
+
+def teardown():
+    browser.quit()
+    stop_server()
+
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
+    setup()
     result = runner.run(test_suite())
+    teardown()
     sys.exit(int(bool(result.failures or result.errors)))
-
