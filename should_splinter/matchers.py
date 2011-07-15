1821e1f9957173950555168b725d69f3d30ec263
bernardofire <bernardo-fire@hotmail.com>
2011-07-15 01:33:32 -0300

    add matcher include_id

--- should_splinter/matchers.py
+++ should_splinter/matchers.py
@@ -5,3 +5,7 @@ def include_text():
     return (lambda browser, text: browser.is_text_present(text),
             '%r does %sinclude text %r')
 
+@matcher
+def include_id():
+    return (lambda browser, id: browser.find_by_id(id),
+            '%r does %sinclude id %r')
